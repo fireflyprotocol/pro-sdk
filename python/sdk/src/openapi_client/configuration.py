e1d4832cb4b232bb4d6a2f14ce5ecbaefc4c6fc8
kevin <kevin@seed.im>
2025-04-10 22:29:16 -0500

    [BFP-1459] Update server url with URL to sui-staging and sui-prod.
    Since we have subdomains for different paths,
    e.g., auth.api.{env}.bluefin.io for /auth/*,
    we need to define a specific server property for each one
    so that, after generating the OpenAPI spec,
    users can interact with the API using the appropriate subdomain.

--- python/sdk/src/openapi_client/configuration.py
+++ python/sdk/src/openapi_client/configuration.py
@@ -186,7 +186,7 @@ class Configuration:
     ) -> None:
         """Constructor
         """
-        self._base_path = "http://localhost:8080" if host is None else host
+        self._base_path = "https://api.sui-staging.bluefin.io" if host is None else host
         """Default Base url
         """
         self.server_index = 0 if server_index is None and host is None else server_index
@@ -510,8 +510,18 @@ class Configuration:
         """
         return [
             {
-                'url': "http://localhost:8080",
+                'url': "https://api.{env}.bluefin.io",
                 'description': "No description provided",
+                'variables': {
+                    'env': {
+                        'description': "No description provided",
+                        'default_value': "sui-staging",
+                        'enum_values': [
+                            "sui-staging",
+                            "sui-prod"
+                        ]
+                        }
+                    }
             }
         ]
 
