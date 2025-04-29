858c67fba90e0a4c24d84a8670603b5d7bc5f060
nidakarimali <nida@seed.im>
2025-04-29 19:11:41 -0400

    reverted

--- python/sdk/src/openapi_client/api/__init__.py
+++ python/sdk/src/openapi_client/api/__init__.py
@@ -4,7 +4,6 @@
 from openapi_client.api.account_data_api import AccountDataApi
 from openapi_client.api.auth_api import AuthApi
 from openapi_client.api.exchange_api import ExchangeApi
-from openapi_client.api.rewards_api import RewardsApi
 from openapi_client.api.streams_api import StreamsApi
 from openapi_client.api.trade_api import TradeApi
 
