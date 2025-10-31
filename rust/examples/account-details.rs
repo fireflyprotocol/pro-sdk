5b504d604ccc13ed17da6f020f7d1831c13be81c
Jeff Schwab <jeff@unbuggy.com>
2025-10-31 13:06:45 -0400

    Print account details

--- rust/examples/account-details.rs
+++ rust/examples/account-details.rs
@@ -7,6 +7,10 @@ type Error = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Error>;
 
 /// Sends a request for account details, and returns the deserialized response.
+///
+/// # Errors
+///
+/// Will return an error if the server cannot be reached.
 async fn send_request(account_address: &str, environment: Environment) -> Result<Account> {
     println!("Sending request...");
     Ok(get_account_details(
@@ -22,8 +26,8 @@ async fn send_request(account_address: &str, environment: Environment) -> Result
 #[tokio::main]
 async fn main() -> Result<()> {
     let environment = Environment::Staging;
-    let test_account_address = environment.test_keys().unwrap().address;
-    send_request(test_account_address, environment).await?;
-
+    let address = environment.test_keys().unwrap().address;
+    let account = send_request(address, environment).await?;
+    println!("{account:#?}");
     Ok(())
 }
