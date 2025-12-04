b2a0b1bd9983579e1323a0c40dc0d72e01abdd60
Jeff Schwab <jeff@unbuggy.com>
2025-12-04 07:50:38 -0500

    Remove most logging

--- rust/examples/create-orders.rs
+++ rust/examples/create-orders.rs
@@ -3,7 +3,7 @@ use bluefin_api::apis::trade_api::post_create_order;
 use bluefin_api::models::{
     AccountDataStream, AccountOrderUpdate, AccountStreamMessage, AccountStreamMessagePayload,
     AccountSubscriptionMessage, CreateOrderRequest, OrderSide, OrderTimeInForce, OrderType,
-    SelfTradePreventionType, SubscriptionResponseMessage, SubscriptionType,
+    SelfTradePreventionType, SubscriptionType,
 };
 use bluefin_api::models::{CreateOrderRequestSignedFields, LoginRequest};
 use bluefin_pro::prelude::*;
@@ -34,7 +34,6 @@ async fn send_request(
     environment: Environment,
     is_colocated: bool,
 ) -> Result<String> {
-    println!("Sending request...");
     // Send request and get back order hash
     let mut config = Configuration::new();
     config.bearer_access_token = Some(auth_token.into());
@@ -61,45 +60,31 @@ async fn handle_websocket(
 ) {
     while !shutdown_flag.load(std::sync::atomic::Ordering::Relaxed) {
         let Ok(message) = timeout(max_time_without_messages, ws_receiver.next()).await else {
-            println!("Websocket receiver task timed out due to inactivity.");
+            eprintln!("Websocket receiver task timed out due to inactivity.");
             return;
         };
         let Some(Ok(message)) = message else {
-            println!("Websocket receiver task terminated");
+            eprintln!("Websocket receiver task terminated");
             return;
         };
         match message {
             Message::Ping(_) => {
-                println!("Ping received");
                 if let Err(error) = ws_sender.send(Message::Pong(Vec::new())).await {
                     eprintln!("Error sending Pong: {error}");
                     return;
                 }
-                println!("Pong sent");
             }
-            Message::Pong(_) => println!("Pong received"),
             Message::Text(text) => {
-                // Broadcast account update, or print subscription response.
-                if let Ok(websocket_message) = serde_json::from_str::<AccountStreamMessage>(&text) {
-                    if let Err(error) = sender.send(websocket_message) {
-                        eprintln!("Error sending message to channel: {error}");
-                        return;
-                    }
-                } else if let Ok(subscription_message) =
-                    serde_json::from_str::<SubscriptionResponseMessage>(&text)
+                if let Ok(websocket_message) = serde_json::from_str::<AccountStreamMessage>(&text)
+                    && let Err(error) = sender.send(websocket_message)
                 {
-                    println!(
-                        "Subscription response message received: {}",
-                        serde_json::to_string_pretty(&subscription_message).unwrap_or(text)
-                    );
+                    eprintln!("Error sending message to channel: {error}");
+                    return;
                 }
             }
-            Message::Close(_) => {
-                println!("Close received");
-                return;
-            }
+            Message::Close(_) => return,
             _ => {
-                eprintln!("Unknown message received");
+                eprintln!("Unknown WebSocket message: {message:?}");
                 return;
             }
         }
@@ -148,38 +133,36 @@ async fn subscribe_to_updates(
 }
 
 async fn handle_channel(mut receiver: Receiver<AccountStreamMessage>) {
-    while let Ok(websocket_message) = receiver.recv().await {
-        let AccountStreamMessage::AccountOrderUpdate {
-            payload: AccountStreamMessagePayload::AccountOrderUpdate(update),
-            ..
-        } = &websocket_message
-        else {
-            eprintln!("Unknown message received {websocket_message:#?}");
+    while let Ok(message) = receiver.recv().await {
+        let AccountStreamMessage::AccountOrderUpdate { payload, .. } = &message else {
+            eprintln!("Unknown account stream message: {message:#?}");
             continue;
         };
-
+        let AccountStreamMessagePayload::AccountOrderUpdate(update) = payload else {
+            eprintln!("Unknown account stream payload: {message:#?}");
+            continue;
+        };
         match update {
-            AccountOrderUpdate::ActiveOrderUpdate(order_update) => {
-                println!("Order {} opened", order_update.order_hash);
-                println!("Account Order Update {order_update:#?}");
+            AccountOrderUpdate::ActiveOrderUpdate(order) => {
+                println!("Order updated: {order:#?}");
                 break;
             }
-            AccountOrderUpdate::OrderCancellationUpdate(order_cancelled) => {
-                eprintln!("Order {} cancelled", order_cancelled.order_hash);
+            AccountOrderUpdate::OrderCancellationUpdate(cancel) => {
+                println!("Order cancelled: {cancel:?}");
                 break;
             }
         }
     }
 }
 
-fn new_request(environment: Environment, ids_id: String) -> CreateOrderRequest {
+fn new_request(environment: Environment, ids_id: String, price_e9: u64) -> CreateOrderRequest {
     CreateOrderRequest {
         signed_fields: CreateOrderRequestSignedFields {
             symbol: "ETH-PERP".to_string(),
             account_address: environment.test_keys().unwrap().address.into(),
-            price_e9: (10_000.e9()).to_string(),
+            price_e9: price_e9.to_string(),
             quantity_e9: (1.e9()).to_string(),
-            side: OrderSide::Short,
+            side: OrderSide::Long,
             leverage_e9: (10.e9()).to_string(),
             is_isolated: false,
             salt: random::<u64>().to_string(),
@@ -220,17 +203,6 @@ async fn main() -> Result<()> {
         .await?
         .access_token;
 
-    // Next, we construct an unsigned request using the exchange's IDS ID.
-    let ids_id = exchange::info::contracts_config(environment).await?.ids_id;
-
-    let request = new_request(environment, ids_id);
-
-    // Then, we sign our order.
-    let request = request.sign(
-        PrivateKey::from_hex(environment.test_keys().unwrap().private_key)?,
-        SignatureScheme::Ed25519,
-    )?;
-
     // Listen to order updates to see when an order has been opened. In this
     // example, we first spawn a handler to read from an internal broadcast
     // channel, and then subscribe to updates via WebSocket. Our WebSocket
@@ -246,18 +218,23 @@ async fn main() -> Result<()> {
         Arc::clone(&shutdown_flag),
     )
     .await?;
-    println!("Waiting for account order updates...");
 
-    println!("auth token: {auth_token}");
+    // Next, we construct an unsigned request using the exchange's IDS ID.
+    let ids_id = exchange::info::contracts_config(environment).await?.ids_id;
+    let request = new_request(environment, ids_id, 10_000.e9());
+
+    // Then, we sign our order.
+    let request = request.sign(
+        PrivateKey::from_hex(environment.test_keys().unwrap().private_key)?,
+        SignatureScheme::Ed25519,
+    )?;
+
     let computed_order_hash = request.clone().compute_hash().unwrap();
     let received_order_hash = send_request(request, &auth_token, environment, false).await?;
 
     // Finally, we check that we've received the expected order hash.
-    println!("Order Submitted: {received_order_hash}");
     assert_eq!(computed_order_hash, received_order_hash);
-    println!("Order hash matches");
 
     handle.await.expect("Could not join handle");
-
     Ok(())
 }
