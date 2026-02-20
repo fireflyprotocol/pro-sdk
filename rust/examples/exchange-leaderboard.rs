45ce713d6ef572b87ab359e32048e764dee83d73
kevin <kevin@seed.im>
2026-02-20 17:11:01 -0600

    rust

---
+++ rust/examples/exchange-leaderboard.rs
@@ -0,0 +1,28 @@
+use bluefin_api::apis::configuration::Configuration;
+use bluefin_api::apis::exchange_api::get_leaderboard;
+use bluefin_api::models;
+use bluefin_pro::prelude::*;
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let environment = Environment::Staging;
+    let response = get_leaderboard(
+        &Configuration {
+            base_path: exchange::url(environment).into(),
+            ..Configuration::default()
+        },
+        Some(models::LeaderboardInterval::LeaderboardIntervalN30d), // interval
+        None, // sort_by
+        None, // sort_order
+        None, // limit
+        None, // page
+    )
+    .await?;
+
+    println!("{response:#?}");
+
+    Ok(())
+}
